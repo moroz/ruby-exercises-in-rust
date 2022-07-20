f68c8460aec69a024138eca04854395ea7edf608
Karol Moroz <k.j.moroz@gmail.com>
2022-07-20 13:12:49 +0200

    Finish implementation of 29

--- 29-todos/29-todos.rs
+++ 29-todos/29-todos.rs
@@ -4,9 +4,59 @@ use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
+use std::path::PathBuf;
 use std::str::FromStr;
 
-type Todos = Vec<String>;
+struct Todos {
+    path: PathBuf,
+    items: Vec<String>,
+}
+
+impl Todos {
+    fn read(filename: impl AsRef<Path>) -> io::Result<Self> {
+        let file = File::open(&filename)?;
+        let file = BufReader::new(file);
+        let mut todos: Vec<String> = Vec::new();
+
+        for line in file.lines() {
+            let line = line?;
+            if line.is_empty() {
+                continue;
+            }
+            todos.push(line);
+        }
+        let mut path = PathBuf::new();
+        path.push(filename);
+
+        Ok(Self {
+            path: path,
+            items: todos,
+        })
+    }
+
+    fn print(&self) {
+        for (index, todo) in self.items.iter().enumerate() {
+            println!("{}: {}", index, todo);
+        }
+    }
+
+    fn add(&mut self, text: String) {
+        self.items.push(text);
+    }
+
+    fn remove(&mut self, index: usize) -> bool {
+        if index < self.items.len() {
+            let _ = &self.items.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn save(&self) {
+        fs::write(&self.path, &self.items.join("\n")).unwrap();
+    }
+}
 
 enum Command {
     Add,
@@ -29,28 +79,6 @@ impl FromStr for Command {
     }
 }
 
-fn read_todos(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-    let file = File::open(filename)?;
-    let file = BufReader::new(file);
-    let mut todos: Todos = Vec::new();
-
-    for line in file.lines() {
-        let line = line?;
-        if line.is_empty() {
-            continue;
-        }
-        todos.push(line);
-    }
-
-    Ok(todos)
-}
-
-fn print_todos(todos: &Todos) {
-    for (index, todo) in todos.iter().enumerate() {
-        println!("{}: {}", index, todo);
-    }
-}
-
 fn gets() -> Option<String> {
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).ok()?;
@@ -65,7 +93,18 @@ fn get_command() -> Option<Command> {
 fn add_todo(todos: &mut Todos) {
     println!("请输入代办事项: ");
     if let Some(text) = gets() {
-        todos.push(text);
+        todos.add(text);
+    }
+}
+
+fn remove_todo(todos: &mut Todos) {
+    println!("请输入要删除的编号: ");
+    if let Some(text) = gets() {
+        if let Ok(index) = text.parse::<usize>() {
+            if !&todos.remove(index) {
+                println!("無法刪除編號{}的項目！", index);
+            }
+        }
     }
 }
 
@@ -74,19 +113,24 @@ fn process_command(todos: &mut Todos, command: Command) {
         Command::Add => {
             add_todo(todos);
         }
+        Command::Save => {
+            todos.save();
+        }
+        Command::Remove => {
+            remove_todo(todos);
+        }
         Command::Quit => {
             std::process::exit(0);
         }
-        _ => (),
     }
 }
 
 fn main() -> io::Result<()> {
     let path = env::current_dir()?.parent().unwrap().join("todos.txt");
-    let mut todos = read_todos(&path)?;
-    print_todos(&todos);
+    let mut todos = Todos::read(&path)?;
 
     loop {
+        todos.print();
         if let Some(command) = get_command() {
             process_command(&mut todos, command);
         }
@@ -94,19 +138,6 @@ fn main() -> io::Result<()> {
 
     Ok(())
 }
-
-// 简易 Todo 代办事项应用
-
-// text = File.read("todos.txt")
-
-// todos = []
-// text.each_line do |line|
-//   todos << line.chomp
-// end
-
-// todos.each_with_index do |todo, index|
-//   puts "#{index}: #{todo}"
-// end
 
 // while (true)
 //   print "请输入指令 1. add 2. remove 3. save，然后按 Enter: "
