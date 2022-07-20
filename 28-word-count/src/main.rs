ce049f68617480a3fd2087d42b36fa2f3cc8061b
Karol Moroz <k.j.moroz@gmail.com>
2022-07-20 11:22:45 +0200

    Implement solution for 28

--- 28-word-count/src/main.rs
+++ 28-word-count/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    let file = File::open("../wordcount.txt")?;
+    let f = BufReader::new(file);
+    let mut counts: HashMap<String, i32> = HashMap::new();
+    let split_regex = Regex::new("[.\\n\\s]+").unwrap();
+
+    for line in f.lines() {
+        for word in split_regex.split(&line.unwrap()) {
+            if word.is_empty() {
+                continue;
+            }
+            let word = word.to_lowercase();
+            if let Some(count) = counts.get_mut(&word) {
+                *count += 1;
+            } else {
+                counts.insert(word, 1);
+            }
+        }
+    }
+
+    let mut sorted: Vec<_> = counts.iter().collect();
+    sorted.sort_by_key(|&(_k, v)| v);
+    sorted.reverse();
+
+    for (k, v) in sorted {
+        println!("{}: {}", k, v);
+    }
+
+    Ok(())
 }
